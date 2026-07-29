b9d188c9cb22e94eaa1010fe764560af2d79963a
Timothy Long <st7gg4wk7b@privaterelay.appleid.com>
2026-07-29 13:23:49 -0400

    add header file for vga driver

---
+++ Include/vga.h
@@ -0,0 +1,9 @@
+static inline void outb(uint16_t port, uint8_t value) {
+    asm volatile("outb %0, %1" : : "a"(value), "Nd"(port));
+}
+
+static inline uint8_t inb(uint16_t port) {
+    uint8_t ret;
+    asm volatile("outb %0, %1" : "=a"(ret) : "Nd"(port));
+    return ret;
+}
