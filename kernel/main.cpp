db412bc1d3056b7095a37a5afe0a97a1bd41a136
Timothy Long <st7gg4wk7b@privaterelay.appleid.com>
2026-07-30 02:05:55 -0400

    use the VGA driver in kernel_main
    drop the inline 0xB8000 loop from before the driver existed. it was overwriting the drivers input on row 0

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -1,14 +1,7 @@
 #include "vga.h"
 
 extern "C" void kernel_main() {
-    volatile char* vga = (char*)0xb8000;
-    const char* msg = "Interval is running";
     terminal_initialize();
     terminal_writestring("Welcome to Interval\n");
-    for (int i = 0; msg[i]; i++) {
-       vga[i * 2] = msg[i];
-        vga[i * 2 + 1] = 0x0F;
-    }
-    while (true) {
-    }
+    while (true) {}
 }
