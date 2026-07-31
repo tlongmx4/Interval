3d08a62aaa63e88104eef63607ae54d49809104a
Timothy Long <st7gg4wk7b@privaterelay.appleid.com>
2026-07-31 12:18:20 -0400

    zero .bss at startup
    linker.ld exports __bss_start and __bss_end. entry.asm zeroes the
    range with rep stosb before calling kernel_main.
    
    Previously .bss was zero only because dd zero-fills disk.img, which
    would have broken as soon as the kernel outgrew the image or the
    layout changed.

--- kernel/entry.asm
+++ kernel/entry.asm
@@ -1,7 +1,16 @@
 bits 32
 global _start
 extern kernel_main
+extern __bss_start
+extern __bss_end
 
 _start:
+    cld
+    mov edi, __bss_start
+    mov ecx, __bss_end
+    sub ecx, edi
+    xor eax, eax
+    rep stosb
+    
     call kernel_main
     jmp $
