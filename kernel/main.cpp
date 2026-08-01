9160c7b2231f398348d7cf8a4d3432c298abdced
Timothy Long <st7gg4wk7b@privaterelay.appleid.com>
2026-07-31 20:03:06 -0400

    declare idt_init and call it from kernel_main
    idt.cpp doesn't exist yet, so this doesn't link. Committing the
    interface separately from the implementation.

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -1,7 +1,14 @@
 #include "vga.h"
+#include "idt.h"
 
 extern "C" void kernel_main() {
     terminal_initialize();
     terminal_writestring("Welcome to Interval\n");
+
+    idt_init();
+    terminal_writestring("IDT installed\n");
+
+    asm volatile("int $3");
+
     while (true) {}
 }
